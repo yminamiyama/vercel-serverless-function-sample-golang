897add493584334caf23b8ea3804173c0c8ddd69
Yukinori Minamiyama <137569967+yminamiyama@users.noreply.github.com>
2024-01-03 10:54:11 +0000

    Adjusted codes for sample.

--- sample/api/v1/hello/index.go
+++ sample/api/v1/hello/index.go
@@ -16,7 +16,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		h := Hello{
 			ID:      "1",
-			Message: "Hello World!",
+			Message: "Hello World!!!!",
 		}
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
