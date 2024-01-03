e5c1e4ebf26977890db3deb73019891dc3fe70ae
Yukinori Minamiyama <137569967+yminamiyama@users.noreply.github.com>
2024-01-03 06:44:29 +0000

    Added initial codes for vercel.

---
+++ sample/api/v1/hello/index.go
@@ -0,0 +1,29 @@
+package hello
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	ID      string `json:"id"`
+	Message string `json:"message"`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		h := Hello{
+			ID:      "1",
+			Message: "Hello World!!",
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		if err := json.NewEncoder(w).Encode(h); err != nil {
+			log.Println(err)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
